cc90ddf2823f8a485651db12ad1a0393bf9cc873
Shane Irvine <113709537+irvinese@users.noreply.github.com>
2024-03-18 17:08:30 -0400

    In the middle of typing getting the test to work but have to stop for the day

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
 describe(`My First Test`, () => {
-  it(`gets, types, and asserts`, () => {
-    // eslint-disable-next-line no-undef
+  it(`types my name`, () => {
+
+    /*
     cy.visit(`https://example.cypress.io`)
 
-    // eslint-disable-next-line no-undef
     cy.contains(`type`).click()
 
     cy.url().should(`include`, `/commands/acitions`)
@@ -12,5 +12,22 @@ describe(`My First Test`, () => {
     cy.get(`.action-email`).type(`fake@email.com`)
 
     cy.get(`.action-email`).should(`have.value`, `fake@email.com`)
+    */
+    cy.visit(`https://www.google.com/`)
+
+    cy.contains(`Search`).click()
+
+    cy.url().should(`include`, `/commands/acitions`)
+
+    cy.get(`Name`).type(`Shane`)
+
+    cy.get(`.dropdwon-selector`).select(`Option 1`)
+
+    cy.get(`.submit-button`).click()
+
+    cy.get(`.history-log`).should(`have.length`, 1)
+
+    cy.get(`.history-log`).contains(`Shane`).should(`exist`)
+
   })
 })
